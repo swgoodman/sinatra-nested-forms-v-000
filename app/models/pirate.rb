a8e2f4165f41739d0c0859c979d05b9df52cebd2
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-10 17:54:32 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -4,9 +4,9 @@ class Pirate
   @@pirates = []
 
   def initialize(name, weight, height)
-    @name = name
-    @weight = weight
-    @height = height
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
   end
 
   def self.all
