39bef98e7debf08e76398d12f3729db99b0b7d9f
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-10 18:37:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,9 +12,7 @@ module FormsLab
     end
 
     post '/pirates' do
-      # Set params equal to variables in the controller
       @pirate = Pirate.new(params[:pirate])
-      # shoot the variables into the show.erb file
       erb :'pirates/show'
     end
   end
