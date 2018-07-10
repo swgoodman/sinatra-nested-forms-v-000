682e559ce43c9b4851b6fd6e9942e288e7ba647d
Spencer Goodman <spencerwgoodman@icloud.com>
2018-07-10 17:58:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,9 +4,7 @@ module FormsLab
   class App < Sinatra::Base
 
     get '/' do
-      "Welcome to the Nested Forms Lab!"
-      "let's navigate to
-the '/new'"
+      "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
     end
 
     get '/new' do
